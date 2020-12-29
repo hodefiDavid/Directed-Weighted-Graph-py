f7c94d0161dbde868f9941e4e5086d6174e587d7
davidfeust <73194236+davidfeust@users.noreply.github.com>
2020-12-29 22:46:59 +0200

    docs

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -4,29 +4,73 @@ from GraphInterface import *
 class DiGraph(GraphInterface):
 
     def __init__(self):
+        """
+        Constructor.
+        init dictionary to stores the nodes of the graph.
+        also init mode_count and edge_size to 0.
+        """
         self.nodes = dict()
         self.mode_count = 0
         self.edge_size = 0
 
     def v_size(self) -> int:
+        """
+        Returns the length of nodes field.
+        @return: the number of vertices in this graph.
+        """
         return len(self.nodes)
 
     def e_size(self) -> int:
+        """
+        Returns the number of edges in this graph
+        @return: The number of edges in this graph
+        """
         return self.edge_size
 
     def get_all_v(self) -> dict:
+        """
+        Returns a dictionary of all the nodes in the Graph,
+        each node is represented using a pair (key, NodeData)
+        @return: dict contains all the nodes in the graph.
+        """
         return self.nodes
 
     def all_in_edges_of_node(self, id1: int) -> dict:
+        """
+        Return a dictionary of all the nodes connected to (into) node_id,
+        each node is represented using a pair (key, weight)
+        @param id1: node id
+        @return: dictionary contains nodes connected to id1
+        """
         return self.nodes[id1].node_in
 
     def all_out_edges_of_node(self, id1: int) -> dict:
+        """
+        Return a dictionary of all the nodes connected from node_id,
+        each node is represented using a pair (key, weight)
+        @param id1: node id
+        @return: dictionary contains nodes connected from id1
+        """
         return self.nodes[id1].node_out
 
     def get_mc(self) -> int:
+        """
+        Returns the current version of this graph,
+        on every change in the graph state - the MC should be increased.
+        @return: The current version of this graph.
+        """
         return self.mode_count
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        """
+        Adds an edge to the graph.
+        If the edge already exists or one of the nodes dose not exists the functions will do nothing.
+        This method adds id2 nodes to node_out of id1, and id1 to node_in of id2.
+        @param id1: The start node of the edge.
+        @param id2: The end node of the edge.
+        @param weight: The weight of the edge.
+        @return: True if the edge was added successfully, False o.w.
+        """
         if id1 not in self.nodes or id2 not in self.nodes or id1 == id2:
             return False
 
@@ -43,6 +87,15 @@ class DiGraph(GraphInterface):
         return True
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        """
+        Adds a node to the graph.
+        If the node id already exists the node will not be added.
+        This method adds new NodeData to the nodes dictionary.
+        @param node_id: The node ID.
+        @param pos: The position of the node.
+        @return: True if the node was added successfully, False o.w.
+
+        """
         if node_id in self.nodes:
             return False
         self.nodes[node_id] = DiGraph.NodeData(node_id, pos)
@@ -50,6 +103,14 @@ class DiGraph(GraphInterface):
         return True
 
     def remove_node(self, node_id: int) -> bool:
+        """
+        Removes a node from the graph.
+        If the node id does not exists the function will do nothing.
+        This method remove the node from nodes dictionary,
+        and also remove all in and out edges.
+        @param node_id: The node ID.
+        @return: True if the node was removed successfully, False o.w.
+        """
         if node_id not in self.nodes:
             return False
 
@@ -66,7 +127,14 @@ class DiGraph(GraphInterface):
         return True
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        """
+        Removes an edge from the graph.
+        If such an edge does not exists the function will do nothing.
+        @param node_id1: The start node of the edge.
+        @param node_id2: The end node of the edge.
+        @return: True if the edge was removed successfully, False o.w.
 
+        """
         if node_id1 not in self.nodes or node_id2 not in self.nodes \
                 or node_id2 not in self.nodes[node_id1].node_out:
             return False
@@ -89,6 +157,12 @@ class DiGraph(GraphInterface):
         return self.edge_size == other.edge_size and self.nodes == other.nodes
 
     class NodeData:
+        """
+        This inner class are represent a node in directed weighted graph - DiGraph.
+        Every NodeData has unique id, and also 2D position, and 2 dictionary:
+        for edges get out from the nodes, and for edges get in to the node.
+        This dictionaries node_id as keys and edge weight as values.
+        """
 
         def __init__(self, node_id: int, pos: tuple = None):
             self.id = node_id
@@ -99,9 +173,19 @@ class DiGraph(GraphInterface):
             self.position = pos
 
         def add_out(self, node_id: int, weight: float):
+            """
+            Add new edge from this node to node_id, with the giving weight.
+            @param node_id: The end node of the edge.
+            @param weight: The weight of the edge.
+            """
             self.node_out[node_id] = weight
 
         def add_in(self, node_id: int, weight: float):
+            """
+            Add new edge from node_id to this node, with the giving weight.
+            @param node_id: The start node of the edge.
+            @param weight: The weight of the edge.
+            """
             self.node_in[node_id] = weight
 
         def __str__(self):
--- src/Ex3_main.py
+++ src/Ex3_main.py
@@ -6,25 +6,21 @@ from GraphAlgo import GraphAlgo
 
 def check():
     """
-        This file represents a simple function name tester.
-        Make sure you run this example to check your naming.
-        ***** output: ******
-        Graph: |V|=4 , |E|=5
-        {0: 0: score inf, 1: 1: score inf, 2: 2: score inf, 3: 3: score inf}
-        {0: 1}
-        {0: 1.1, 2: 1.3, 3: 10}
-        (3.4, [0, 1, 2, 3]) ==> mistake (only if remove edge (1,3))! need to be: (2.8, [(0), (1), (3)])
-        [[0, 1], [2], [3]]
-        (3.4, [0, 1, 2, 3])
-        (inf, None)
-        4.244296649514735 [1, 9, 10, 11, 7]
-        20.546312400537253 [47, 46, 45, 44, 43, 42, 41, 40, 39, 15, 27, 26, 25, 17, 18, 19]
-        18.197159401343114 [20, 21, 32, 31, 30, 29, 38, 14, 13, 12, 11, 9, 1, 0, 2]
-        inf None
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-        [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]]
-
-        """
+    Graph: |V|=4 , |E|=5
+    {0: 0: score inf, 1: 1: score inf, 2: 2: score inf, 3: 3: score inf}
+    {0: 1}
+    {0: 1.1, 2: 1.3, 3: 10}
+    (11, [0, 1, 3])
+    [[0, 1], [2], [3]]
+    (2.8, [0, 1, 3])
+    (inf, None)
+    2.062180280059253 [1, 10, 7]
+    17.693921758901507 [47, 46, 44, 43, 42, 41, 40, 39, 15, 16, 17, 18, 19]
+    11.51061380461898 [20, 21, 32, 31, 30, 29, 14, 13, 3, 2]
+    inf None
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]]
+    """
     check0()
     check1()
     check2()
@@ -50,6 +46,7 @@ def check0():
     print(g.all_in_edges_of_node(1))
     print(g.all_out_edges_of_node(1))
     g_algo = GraphAlgo(g)
+    g_algo.save_to_json("../data/EEE.json")
     print(g_algo.shortest_path(0, 3))
 
 
@@ -61,13 +58,7 @@ def check1():
     g_algo = GraphAlgo()  # init an empty graph - for the GraphAlgo
     file = "../data/T0.json"
     g_algo.load_from_json(file)  # init a GraphAlgo from a json file
-    # g_algo.graph.remove_edge(1, 3)
-    print(g_algo.connected_component(0), 0)
-    print(g_algo.connected_component(1), 1)
-    print(g_algo.connected_component(2), 2)
-    print(g_algo.connected_component(3), 3)
     print(g_algo.connected_components())
-    # print(g_algo.graph)
     print(g_algo.shortest_path(0, 3))
     print(g_algo.shortest_path(3, 1))
     g_algo.save_to_json(file + '_saved')
@@ -83,7 +74,6 @@ def check2():
     g_algo.load_from_json(file)
     g_algo.get_graph().remove_edge(13, 14)
     g_algo.save_to_json(file + "_edited")
-    # print(g_algo.get_graph())
     dist, path = g_algo.shortest_path(1, 7)
     print(dist, path)
     dist, path = g_algo.shortest_path(47, 19)
