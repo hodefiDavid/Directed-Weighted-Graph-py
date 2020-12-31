03250d7243154a47cfd657e69b6f4dcefbf7225a
davidfeust <73194236+davidfeust@users.noreply.github.com>
2021-01-01 00:37:53 +0200

    algo works with docs

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,24 +1,19 @@
 import json
 from heapq import heappush, heappop
-
 from DiGraph import *
 from GraphAlgoInterface import *
 from Gui import Gui
 
 
 class GraphAlgo(GraphAlgoInterface):
-
-
     """This class represents an algorithms class for the class Digraph."""
-
 
     def __init__(self, g: DiGraph = None):
         """
-        initialize the graph that this class work on
+        Initialize the graph that this class work on.
         :param g: graph DiGraph
         """
         self.graph = g
-
 
     def get_graph(self) -> GraphInterface:
         """
@@ -26,12 +21,11 @@ class GraphAlgo(GraphAlgoInterface):
         """
         return self.graph
 
-
     def load_from_json(self, file_name: str) -> bool:
         """
-         Loads a graph from a json file.
+        Loads a graph from a json file.
 
-        :param file_name: The path to the json file
+        :param: file_name: The path to the json file.
         :return: True if the loading was successful, False o.w.
         """
         try:
@@ -52,8 +46,7 @@ class GraphAlgo(GraphAlgoInterface):
             print(e)
             return False
 
-            return True
-
+        return True
 
     def save_to_json(self, file_name: str) -> bool:
         """
@@ -61,7 +54,7 @@ class GraphAlgo(GraphAlgoInterface):
 
         :param self: getting the self of this class
         :param file_name: The path to the out file
-        :return: True if the save was successful, Flase o.w.
+        :return: True if the save was successful, False o.w.
         """
         j = dict()
         j["Edges"] = list()
@@ -81,7 +74,6 @@ class GraphAlgo(GraphAlgoInterface):
         except IOError:
             return False
 
-
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         """
         Returns the shortest path from node id1 to node id2 using Dijkstra's Algorithm
@@ -92,31 +84,30 @@ class GraphAlgo(GraphAlgoInterface):
         :return: The distance of the path, the path as a list
         """
         if id1 not in self.graph.nodes.keys() or id2 not in self.graph.nodes.keys():
-            return -1, None
+            return -1, []
         src = self.graph.nodes[id1]
         dest = self.graph.nodes[id2]
 
-            if id1 == id2:
-                return 0, [src.id]
+        if id1 == id2:
+            return 0, [src.id]
 
-            self.set_tag_dist(id1)
-            if dest.tag == -1:
-                return float("inf"), None
+        self.set_tag_dist(id1)
+        if dest.tag == -1:
+            return float("inf"), None
 
-            lst = []
-            lst.insert(0, dest.id)
-            curr = lst[0]
+        lst = []
+        lst.insert(0, dest.id)
+        curr = lst[0]
 
-            while curr != src.id:
-                for i, w in self.graph.nodes[curr].node_in.items():
-                    n = self.graph.nodes[i]
-                    if n.tag + w == self.graph.nodes[curr].tag:
-                        lst.insert(0, n.id)
-                        curr = lst[0]
-                        break
+        while curr != src.id:
+            for i, w in self.graph.nodes[curr].node_in.items():
+                n = self.graph.nodes[i]
+                if n.tag + w == self.graph.nodes[curr].tag:
+                    lst.insert(0, n.id)
+                    curr = lst[0]
+                    break
 
-            return dest.tag, lst
-
+        return dest.tag, lst
 
     def set_tag_dist(self, id1):
         """
@@ -137,72 +128,70 @@ class GraphAlgo(GraphAlgoInterface):
                     n.tag = curr.tag + w
                     heappush(p_queue, n)
 
-
     def init_tags(self):
         """
-        initialize all the tag's nodes in the graph
-        :param self: getting the self of this class
+        Initialize all the tag's nodes in the graph.
+        :param self: getting the self of this class.
         """
         for i in self.graph.nodes.values():
             i.tag = -1
-
 
     def connected_component(self, id1: int) -> list:
         """
         Finds the Strongly Connected Component(SCC) that node id1 is a part of.
 
-        :param self:getting the self of this class
-        :param id1:The node id
-        :return:The list of nodes in the SCC
+        :param self: getting the self of this class
+        :param id1: The node id
+        :return: The list of nodes in the SCC
         """
         if id1 not in self.graph.nodes.keys():
             return None
 
-            self.init_tags()
-            num = 0
-            self.set_connected_tag(self.graph.nodes[id1], num)
-            ll = [id1]
-            for i in self.graph.nodes.values():
-                if i.tag == 0:
-                    ll.append(i.id)
-            res = []
-            for i in ll:
-                num += 1
-                if self.connect_to_src(id1, i, num):
-                    res.append(i)
-            return res
+        self.init_tags()
+        num = 0
+        self.set_connected_tag(self.graph.nodes[id1], num)
+        ll = [id1]
+        for i in self.graph.nodes.values():
+            if i.tag == 0:
+                ll.append(i.id)
+        res = []
+        for i in ll:
+            num += 1
+            if self.connect_to_src(id1, i, num):
+                res.append(i)
+        return res
 
-        def set_connected_tag(self, src: DiGraph.NodeData, num):
-            pq = []
-            src.tag = -2
-            heappush(pq, src)
-            while len(pq) > 0:
-                temp = heappop(pq)
-                for i in temp.node_out.keys():
-                    t_node = self.graph.nodes[i]
-                    if t_node.tag == -1:
-                        t_node.tag = num
-                        heappush(pq, t_node)
+    def set_connected_tag(self, src: DiGraph.NodeData, num):
+        pq = []
+        src.tag = -2
+        heappush(pq, src)
+        while len(pq) > 0:
+            temp = heappop(pq)
+            for i in temp.node_out.keys():
+                t_node = self.graph.nodes[i]
+                if t_node.tag == -1:
+                    t_node.tag = num
+                    heappush(pq, t_node)
 
-        def connect_to_src(self, src: int, node: int, num: int):
-            if src == node:
-                return True
-            lst = []
-            node_ = self.graph.nodes[node]
-            node_.tag = num
-            lst.append(node_)
+    def connect_to_src(self, src: int, node: int, num: int):
+        if src == node:
+            return True
+        lst = []
+        node_ = self.graph.nodes[node]
+        node_.tag = num
+        lst.append(node_)
 
-            while len(lst) > 0:
-                temp = lst.pop(0)
-                for i in temp.node_out.keys():
-                    t_node = self.graph.nodes[i]
-                    if t_node.tag != num:
-                        if t_node.tag == -2:
-                            node_.tag = -2
-                            return True
-                        t_node.tag = num
-                        lst.append(t_node)
-            return False
+        while len(lst) > 0:
+            temp = lst.pop(0)
+            for i in temp.node_out.keys():
+                t_node = self.graph.nodes[i]
+                if t_node.tag != num:
+                    if t_node.tag == -2:
+                        node_.tag = -2
+                        return True
+                    t_node.tag = num
+                    lst.append(t_node)
+        return False
 
     def connected_components(self) -> List[list]:
         """
@@ -210,6 +199,7 @@ class GraphAlgo(GraphAlgoInterface):
         :param self: getting the self of this class
         :return: The list all SCC
         """
+        set_ = set()
         lst = []
         for n in self.graph.nodes.values():
             temp = self.connected_component(n.id)
@@ -227,7 +217,6 @@ class GraphAlgo(GraphAlgoInterface):
                     lst.append(ll)
             return lst
 
-
     def plot_graph(self) -> None:
         """
         Plots the graph.
@@ -237,7 +226,6 @@ class GraphAlgo(GraphAlgoInterface):
         :return: None
         """
         Gui(self.graph)
-
 
     @staticmethod
     def list_equals(lst1, lst2):
